c50c261eed73f7ff0f26101a75bee913d9388af5
Vhex <Vhex@users.noreply.github.com>
2016-01-11 11:54:07 -1000

    Changed paths to use os.path.join, changed file check to be os.path.isfile, added else for WinMerge being in the path.

--- WinMerge.py
+++ WinMerge.py
@@ -1,4 +1,5 @@
 import sublime_plugin
+import sublime
 import os
 from subprocess import Popen
 try:
@@ -6,15 +7,18 @@ try:
 except:
     import winreg
 
-WINMERGE = winreg.QueryValue(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\WinMergeU.exe')
-
-if not WINMERGE:
-    if os.path.exists("%s\WinMerge\WinMergeU.exe" % os.environ['ProgramFiles(x86)']):
-        WINMERGE = '"%s\WinMerge\WinMergeU.exe"' % os.environ['ProgramFiles(x86)']
-    elif os.path.exists("%s\WinMerge\WinMergeU.exe" % os.environ['ProgramFiles']):
-        WINMERGE = '"%s\WinMerge\WinMergeU.exe"' % os.environ['ProgramFiles']
+try:
+    WINMERGE = winreg.QueryValue(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\WinMergeU.exe')
+except:
+    if os.path.isfile(os.path.join(os.environ['ProgramFiles(x86)'], "WinMerge", "WinMergeU.exe")):
+        WINMERGE = os.path.join(os.environ['ProgramFiles(x86)'], "WinMerge", "WinMergeU.exe")
+    elif os.path.isfile(os.path.join(os.environ['ProgramFiles'], "WinMerge", "WinMergeU.exe")):
+        WINMERGE = os.path.join(os.environ['ProgramFiles'], "WinMerge", "WinMergeU.exe")
+    elif os.path.isfile(os.path.join(os.path.dirname(os.path.dirname(sublime.executable_path())), "WinMergePortable", "WinMergePortable.exe")):
+        WINMERGE = os.path.join(os.path.dirname(os.path.dirname(sublime.executable_path())), "WinMergePortable", "WinMergePortable.exe")
     else:
-        WINMERGE = '"%s\WinMergePortable\WinMergePortable.exe"' % os.path.dirname(os.path.dirname(sublime.executable_path()))
+        # Hope it's in the path
+        WINMERGE = '"WinMergeU.exe"'
 
 fileA = fileB = None
 
